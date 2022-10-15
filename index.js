7857983418f2eb27a54e3da6e75c8ce67fc46b23
Mateus Sarmento <mateus.sarmento@ee.ufcg.edu.br>
2022-10-15 13:13:43 -0300

    fixing db url

--- index.js
+++ index.js
@@ -1,43 +1,28 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const routes = require('./src/routes');
-// require('dotenv').config();
-// const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./src/routes');
+require('dotenv').config();
+const cors = require('cors');
 
-// const mongoString = process.env.DATABASE_URL;
-// mongoose.connect(mongoString);
-// const database = mongoose.connection;
+const uri = process.env.DATABASE_URL;
+// const uri = "mongodb+srv://mateussarmento:123qwe@cluster0.io8uqxl.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri);
+const database = mongoose.connection;
 
-// database.on('error', (error) => {
-//     console.log(error);
-// })
-
-// database.once('connected', () => {
-//     console.log('Database Connected');
-// })
-
-// const app = express();
-
-// // enabling CORS for all requests
-// app.use(cors());
-// app.use(express.json());
-// app.use('/', routes)
-
-// app.listen(process.env.PORT || 3000)
-
-// create an express app
-const express = require("express")
-const app = express()
-
-// use the express-static middleware
-app.use(express.static("public"))
-
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+database.on('error', (error) => {
+    console.log(error);
 })
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
-    
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+
+// enabling CORS for all requests
+app.use(cors());
+app.use(express.json());
+app.use('/', routes)
+
+app.listen(process.env.PORT || 3001)
+
